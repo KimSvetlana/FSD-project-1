74b69b897f91226f85946166f6afe8cfa88fd72f
Sveta <svetlanamingalieva@gmail.com>
2020-02-06 20:48:34 +0700

    изменение в подключении календаря

--- src/index.js
+++ src/index.js
@@ -7,21 +7,5 @@ import './blocks/button/button.js';
 import './blocks/pagination/pagination.js';
 import './blocks/quantityBlock/quantityBlock.js'
 import './blocks/dropdown/dropdown.js';
-
-
-
-
-// import 'item-quantity-dropdown';
-// import 'item-quantity-dropdown/lib/item-quantity-dropdown.css';
-// require('item-quantity-dropdown');
-// require('item-quantity-dropdown/lib/item-quantity-dropdown.css');
-
-// $(document).ready (
-//     function () {
-//         add(2, 4);
-//     }
-// );
-// var j = jQuery.noConflict();
-// j( function() {
-// j( "#calendar" ).datepicker();
-// } );
+import './blocks/airDatepicker/js/datepicker.min.js'
+import './blocks/datepicker/datepicker.js'
