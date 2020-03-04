b7b69375337478f03151c0dd3984b258d81d2f63
Sveta <svetlanamingalieva@gmail.com>
2020-03-04 12:00:16 +0700

    вынесла список чекбоксов

---
+++ src/blocks/expandableCheckboxList/expandableCheckboxList.js
@@ -0,0 +1,15 @@
+$('.expandable-checkbox-list > li').on('click', function(){
+    /* внутри нашего пункта меню находим "выпадашку" */
+    var $dropMenu = $(this).parent().find('.sub-menu');
+     /* если наш dropMenu видимый - скрываем, если скрыт - показываем */
+    if($dropMenu.is(':visible')){
+        $dropMenu.hide();
+    } else {
+    /* скрываем все видимые "выпадашки" на странице */
+        $('.dropMenu:visible').hide(); 
+        $dropMenu.show();
+    }
+        
+        var icon = $('#checkbox-icon');
+        icon.toggleClass('open');
+});
--- src/pages/filter/filter.js
+++ src/pages/filter/filter.js
@@ -1,16 +1,16 @@
-$('.expandable-checkbox-list > li').on('click', function(){
-    /* внутри нашего пункта меню находим "выпадашку" */
-    var $dropMenu = $(this).parent().find('.sub-menu');
-     /* если наш dropMenu видимый - скрываем, если скрыт - показываем */
-    if($dropMenu.is(':visible')){
-        $dropMenu.hide();
-    } else {
-    /* скрываем все видимые "выпадашки" на странице */
-        $('.dropMenu:visible').hide(); 
-        $dropMenu.show();
-    }
+// $('.expandable-checkbox-list > li').on('click', function(){
+//     /* внутри нашего пункта меню находим "выпадашку" */
+//     var $dropMenu = $(this).parent().find('.sub-menu');
+//      /* если наш dropMenu видимый - скрываем, если скрыт - показываем */
+//     if($dropMenu.is(':visible')){
+//         $dropMenu.hide();
+//     } else {
+//     /* скрываем все видимые "выпадашки" на странице */
+//         $('.dropMenu:visible').hide(); 
+//         $dropMenu.show();
+//     }
         
-        var icon = $('#checkbox-icon');
-        icon.toggleClass('open');
-});
+//         var icon = $('#checkbox-icon');
+//         icon.toggleClass('open');
+// });
 
